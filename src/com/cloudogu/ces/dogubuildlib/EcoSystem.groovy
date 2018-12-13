382af031fc2cd42249ab653655a7b64ca98b6956
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2018-12-13 10:20:07 +0100

    removed workaround for verify report

--- src/com/cloudogu/ces/dogubuildlib/EcoSystem.groovy
+++ src/com/cloudogu/ces/dogubuildlib/EcoSystem.groovy
@@ -70,9 +70,8 @@ class EcoSystem {
             script.sh 'rm -f verify.xml'
         }
         try {
-            vagrant.ssh "mkdir -p /tmp/reports/goss_official"
             vagrant.ssh "sudo cesapp verify --health-timeout 600 --keep-container --ci --report-directory=/tmp/reports ${doguPath}"
-            String verifyReport = vagrant.sshOut "cat /tmp/reports/goss_official/*.xml"
+            String verifyReport = vagrant.sshOut "cat /tmp/reports/*.xml"
             script.writeFile encoding: 'UTF-8', file: 'verify.xml', text: verifyReport
         } finally {
             script.junit allowEmptyResults: true, testResults: 'verify.xml'
