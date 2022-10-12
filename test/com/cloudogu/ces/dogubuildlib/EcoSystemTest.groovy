fe07b8f8c95cf7149f151ff90fbe2b9f0811c881
Joshua Sprey <joshua.sprey@cloudogu.com>
2022-10-12 08:02:43 +0200

    #28 Adds unit test for admin group change functionality

--- test/com/cloudogu/ces/dogubuildlib/EcoSystemTest.groovy
+++ test/com/cloudogu/ces/dogubuildlib/EcoSystemTest.groovy
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
+import static org.assertj.core.api.Assertions.assertThat
+import static org.mockito.Mockito.*
+
 @RunWith(MockitoJUnitRunner.class)
 class EcoSystemTest {
 
@@ -60,9 +63,9 @@ class EcoSystemTest {
 
     @Test
     void shouldJoinDependencies() {
-        def config = [ dependencies: ["registrator", "ldap"], additionalDependencies: ["cas"] ]
+        def config = [dependencies: ["registrator", "ldap"], additionalDependencies: ["cas"]]
         def deps = ecoSystem.joinDependencies(config)
-        assert deps == [ "registrator", "ldap", "cas" ]
+        assert deps == ["registrator", "ldap", "cas"]
     }
 
     @Test
@@ -86,7 +89,7 @@ class EcoSystemTest {
     }
 
     @Test
-    void testIncreaseVersion(){
+    void testIncreaseVersion() {
         String start = "2.222.4-1"
         String expected = "2.222.4-2"
         String result = EcoSystem.increaseDoguReleaseVersionByOne("\"Version\": \"${start}\",")
@@ -99,7 +102,7 @@ class EcoSystemTest {
     }
 
     @Test
-    void testparseAdditionalIntegrationTestArgs(){
+    void testparseAdditionalIntegrationTestArgs() {
         def input = ['ARG1=value1', 'ARG2=value2']
         String expected = "-e ARG1=value1 -e ARG2=value2"
         String result = EcoSystem.parseAdditionalIntegrationTestArgs(input)
@@ -109,6 +112,44 @@ class EcoSystemTest {
         expected = ""
         result = EcoSystem.parseAdditionalIntegrationTestArgs(input)
         assert expected == result
+    }
+
+    @Test
+    void test_EcoSystem_prepareGlobalAdminGroupChangeTest() {
+        // given
+        def scriptMock = new ScriptMock()
+        def cypressJson = [
+                "baseUrl": "https://192.168.56.2",
+                "env"    : [
+                        "DoguName"       : "jenkins",
+                        "MaxLoginRetries": 3,
+                        "AdminUsername"  : "ces-admin",
+                        "AdminPassword"  : "ecosystem2016",
+                        "AdminGroup"     : "CesAdministrators"
+                ]
+        ]
+        scriptMock.jsonFiles.put("integrationTests/cypress.json", cypressJson)
+        scriptMock.files.put("integrationTests/cypress.json", cypressJson.toString())
+        scriptMock.expectedShRetValueForScript.put("curl --insecure --silent --head https://192.168.56.2/jenkins | head -n 1", "302")
+
+        EcoSystem sut = new EcoSystem(scriptMock, "gCloudCred", "sshCred")
+        sut.externalIP = "192.168.56.2"
+
+        def vagrantMock = mock(Vagrant.class)
+        doNothing().when(vagrantMock).ssh(any())
+        sut.vagrant = vagrantMock
+
+        // when
+        sut.prepareGlobalAdminGroupChangeTest("jenkins")
+
+        // then
+        assertThat(scriptMock.allActualArgs.get(0)).isEqualTo("curl -u ces-admin:ecosystem2016 --insecure -X POST https://192.168.56.2/usermgt/api/groups -H 'accept: */*' -H 'Content-Type: application/json' -d '{\"description\": \"New admin group for testing\", \"members\": [\"ces-admin\"], \"name\": \"newTestingAdminGroup\"}'")
+        assertThat(scriptMock.writeFileParams.get(0)["text"]).isEqualTo("[baseUrl:https://192.168.56.2, env:[DoguName:jenkins, MaxLoginRetries:3, AdminUsername:ces-admin, AdminPassword:ecosystem2016, AdminGroup:newTestingAdminGroup]]")
+
+        verify(vagrantMock).ssh("etcdctl set /config/_global/admin_group newTestingAdminGroup")
+        verify(vagrantMock).ssh("sudo docker restart jenkins")
+        verify(vagrantMock).ssh("sudo cesapp healthy --wait --timeout 1200 --fail-fast jenkins")
+        verifyNoMoreInteractions(vagrantMock)
     }
 
 }
