25d895ef9e6541ba9ea274243a2bd2b051e52145
Marco Bergen <marco.bergen-extern@cloudogu.com>
2025-01-15 08:32:49 +0100

    [#64] ignore runtime exceptions

--- src/com/cloudogu/ces/dogubuildlib/EcoSystem.groovy
+++ src/com/cloudogu/ces/dogubuildlib/EcoSystem.groovy
@@ -220,7 +220,7 @@ class EcoSystem {
             vagrant.ssh "sudo cesapp login '${escToken}' '${script.env.TOKEN_SECRET}'"
             try {
                 vagrant.ssh "sudo cesapp push ${doguPath}"
-            } catch (Exception ex) {  // NOSONAR this step should only make the build "unstable" instead of "failed"
+            } catch (RuntimeException ex) {  //this step should only make the build "unstable" instead of "failed"
                 script.unstable "Failed to push Dogu to Prerelease-Namespace: ${ex.message}"
             }
         }
