a771016cd8fcb7770f80b5f972aa3ad258ab1e32
Florian Hübner <florian.huebner@cloudogu.com>
2026-09-08 13:24:08 +0200

    [#96] Replace hardcoded machine-type default with config entry

--- src/com/cloudogu/ces/dogubuildlib/MultiNodeEcoSystem.groovy
+++ src/com/cloudogu/ces/dogubuildlib/MultiNodeEcoSystem.groovy
@@ -282,7 +282,7 @@ spec:
         List dogusToAdd = config.additionalDogus
         List componentsToAdd = config.additionalComponents
         def defaultMNParams = """
-MN-CES Machine Type: "e2-standard-4"
+MN-CES Machine Type: "${config.machineType}"
 MN-CES Node Count: "${config.nodeCount}"
 CES Namespace: "ecosystem"
 Ecosystem-Core Chart Namespace: "k8s"
