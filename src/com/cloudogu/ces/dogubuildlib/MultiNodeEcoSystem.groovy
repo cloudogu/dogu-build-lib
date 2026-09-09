11f9456dfabee1797937401b25940568077f9ced
Florian Hübner <florian.huebner@cloudogu.com>
2026-09-09 09:59:43 +0200

    Set default machine type to n2-standard-4

--- src/com/cloudogu/ces/dogubuildlib/MultiNodeEcoSystem.groovy
+++ src/com/cloudogu/ces/dogubuildlib/MultiNodeEcoSystem.groovy
@@ -37,7 +37,7 @@ class MultiNodeEcoSystem extends EcoSystem {
     def multinodeConfig = [
             additionalDogus: [],
             additionalComponents: [],
-            machineType: "n1-standard-4",
+            machineType: "n2-standard-4",
             nodeCount: "1"
     ]
 
